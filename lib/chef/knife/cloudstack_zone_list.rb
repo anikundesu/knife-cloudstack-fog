33b1e20991ba0d7fc4e2c339319f6cc900929476
Jeff Moody <fifthecho@gmail.com>
2012-03-11 00:50:55 -0500

    Added zone list

--- lib/chef/knife/cloudstack_zone_list.rb
+++ lib/chef/knife/cloudstack_zone_list.rb
@@ -20,35 +20,36 @@ require 'chef/knife/cloudstack_base'
 
 class Chef
   class Knife
-    class CloudstackDiskofferingsList < Knife
+    class CloudstackZonesList < Knife
 
       include Knife::CloudstackBase
 
-      banner "knife cloudstack diskofferings list (options)"
+      banner "knife cloudstack zones list (options)"
             
       def run
         $stdout.sync = true
 
         validate!
 
-        diskoffering_list = [
+        zone_list = [
           ui.color('ID', :bold),
           ui.color('Name', :bold),
           ui.color('Description', :bold),
           ui.color('Disk Size (in GB)', :bold)
         ]
-        response = connection.list_disk_offerings['listdiskofferingsresponse']
-          if diskofferings = response['diskoffering']
-            diskofferings.each do |diskoffering|
-              diskoffering_list << diskoffering['id'].to_s
-              diskoffering_list << diskoffering['name'].to_s
-              diskoffering_list << diskoffering['displaytext'].to_s
-              disk_size = diskoffering['disksize']
-              diskoffering_list << disk_size.to_s
+        response = connection.list_zones['listzonesresponse']
+        puts response
+          if zones = response['zone']
+            zones.each do |zone|
+              zone_list << zone['id'].to_s
+              zone_list << zone['name'].to_s
+              zone_list << zone['displaytext'].to_s
+              disk_size = zone['disksize']
+              zone_list << disk_size.to_s
 
             end
           end
-        puts ui.list(diskoffering_list, :columns_across, 4)
+        puts ui.list(zone_list, :columns_across, 4)
 
       end
         
